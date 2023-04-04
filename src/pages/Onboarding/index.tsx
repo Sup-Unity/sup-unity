dcfe3d3ea4e1aec568e69296fec1ab44a343d413
Kyle Olsen <kyle.olsen222@gmail.com>
2023-04-03 20:33:15 -0700

    changed Onboarding page to use css module

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import "./Onboarding.css";
+import "./Onboarding.module.css";
 
 import UserContext from "../../context/user-context";
 
