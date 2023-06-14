95bc937b53250dfd23b479aeff5e1ed64daaa27a
Kyle Olsen <kyle.olsen222@gmail.com>
2023-06-13 19:47:24 -0700

    added Ariah's CommentCard component

---
+++ frontend/src/components/CommentCard/CommentCard.jsx
@@ -0,0 +1,38 @@
+import "./commentcard.css";
+
+import { BiComment } from "react-icons/bi";
+import { AiOutlineLike } from "react-icons/ai";
+
+import edinburgh from "../../tempData/images/edinburgh.jpg";
+
+const jpgMap = {
+  edinburgh,
+};
+
+export default function CommentCard({ comment, profile_img, username }) {
+  return (
+    <div className="comment-card">
+      <div className="comment-header">
+        <div>
+          <img
+            className="profile-img"
+            src={jpgMap[profile_img]}
+            alt={profile_img}
+          />
+          <a href="#">{username}</a>
+        </div>
+        <div className="reply">
+          <div className="icon_and_label">
+            <AiOutlineLike />
+            Like
+          </div>
+          <div className="icon-and-label">
+            <BiComment />
+            Comment
+          </div>
+        </div>
+      </div>
+      <div className="comment-body">{comment}</div>
+    </div>
+  );
+}
